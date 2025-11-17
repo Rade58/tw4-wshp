283a385ec9ff476d31404f610e7851f22af57afe
rade <bajic.rade2@gmail.com>
2025-11-17 20:45:29 +0100

    exercise 1

--- src/components/exercises_has_and_group-has/One.tsx
+++ src/components/exercises_has_and_group-has/One.tsx
@@ -1,4 +1,5 @@
 import type { ComponentProps } from "react";
+import { PrimaryButton } from "../button";
 
 type Props = ComponentProps<"input"> & {
   label?: string;
@@ -8,11 +9,12 @@ type Props = ComponentProps<"input"> & {
 export function One({
   label = "A very important Checkbox",
   id = "checkbox",
+  checked: _checked,
 
   ...props
 }: Props) {
   return (
-    <div className="flex items-center gap-3 rounded-lg border border-slate-200 p-4 has-[input:checked]:border-emerald-800 has-[input:checked]:bg-emerald-300">
+    <div className="group flex flex-wrap items-center gap-3 rounded-lg border border-slate-200 p-4 has-[input:checked]:border-emerald-800 has-[input:checked]:bg-emerald-300">
       <input
         className="h-4 w-4 accent-indigo-800"
         type="checkbox"
@@ -22,6 +24,17 @@ export function One({
       <label className="" htmlFor={id}>
         {label}
       </label>
+      <div className="hidden w-full gap-4 group-has-[input[type='checkbox']:checked]:flex">
+        <input
+          type="text"
+          className="border border-slate-600 px-2 py-3"
+          required
+        />
+        <PrimaryButton
+          label="Submit"
+          className="hidden group-has-[input[type='text']:valid]:block"
+        />
+      </div>
     </div>
   );
 }
