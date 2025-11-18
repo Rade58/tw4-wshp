2e91a3a6ff707911f2611bf938d04872ef591f6b
rade <bajic.rade2@gmail.com>
2025-11-18 22:47:53 +0100

    foo

--- src/components/dark_mode_exercise/Card.tsx
+++ src/components/dark_mode_exercise/Card.tsx
@@ -7,7 +7,7 @@ type Props = PropsWithChildren & {
 
 export function Card({ title = "Title", description = "" }: Props) {
   return (
-    <div className="rounded border border-slate-400 bg-slate-50 p-4 dark:bg-slate-800">
+    <div className="dark:bg-brandfoo rounded border border-slate-400 bg-slate-50 p-4">
       <h2 className="text-lg font-semibold">{title}</h2>
       <p className="mt-1">{description}</p>
     </div>
